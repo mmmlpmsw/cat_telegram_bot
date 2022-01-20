7e83ed60f7f7954462f42de2395c61e87012772e
mmmlpmsw <mmmlpmsw@gmail.com>
2022-01-21 00:45:31 +0300

    Fix ipynb with faces generation Add worry cats generator Add script for launch cats pictures generator

--- python/2nd/faces_generator.py
+++ python/2nd/faces_generator.py
@@ -2,24 +2,10 @@
 # coding: utf-8
 from __future__ import print_function
 
-# import tqdm.autonotebook
-
-# tqdm.autonotebook.get_ipython().run_line_magic('matplotlib', 'inline')
-
-# !pip3 install torch (????)
-# !pip3 install torchvision
-
-# %matplotlib inline
-import argparse
-import random
-
 import torch.nn as nn
 import torch.nn.parallel
-import torch.backends.cudnn as cudnn
 import torch.optim as optim
 import torch.utils.data
-import torchvision.datasets as dset
-import torchvision.transforms as transforms
 import torchvision.utils as vutils
 import numpy as np
 import os
@@ -43,20 +29,8 @@ lr = 0.0002
 beta1 = 0.5
 ngpu = 1
 
-dataset = dset.ImageFolder(root='/Users/nnaumova/Documents/7th semester/catBot/python/2nd/data',
-                           transform=transforms.Compose([
-                               transforms.Resize(image_size),
-                               transforms.CenterCrop(image_size),
-                               transforms.ToTensor(),
-                               transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
-                           ]))
-
-dataloader = torch.utils.data.DataLoader(dataset, batch_size=batch_size,
-                                         shuffle=True, num_workers=workers)
 
 device = torch.device("cuda:0" if (torch.cuda.is_available() and ngpu > 0) else "cpu")
-
-# real_batch = next(iter(dataloader))
 
 
 def weights_init(m):
@@ -154,92 +128,6 @@ G_losses = []
 D_losses = []
 iters = 0
 
-
-# print("Starting Training Loop...")
-# For each epoch
-# for epoch in range(num_epochs):
-#     # For each batch in the dataloader
-#     for i, data in enumerate(dataloader, 0):
-#
-#         ############################
-#         # (1) Update D network: maximize log(D(x)) + log(1 - D(G(z)))
-#         ###########################
-#         ## Train with all-real batch
-#         netD.zero_grad()
-#         # Format batch
-#         real_cpu = data[0].to(device)
-#         b_size = real_cpu.size(0)
-#         label = torch.full((b_size,), real_label, dtype=torch.float, device=device)
-#         # Forward pass real batch through D
-#         output = netD(real_cpu).view(-1)
-#         # Calculate loss on all-real batch
-#         errD_real = criterion(output, label)
-#         # Calculate gradients for D in backward pass
-#         errD_real.backward()
-#         D_x = output.mean().item()
-#
-#         ## Train with all-fake batch
-#         # Generate batch of latent vectors
-#         noise = torch.randn(b_size, nz, 1, 1, device=device)
-#         # Generate fake image batch with G
-#         fake = netG(noise)
-#         label.fill_(fake_label)
-#         # Classify all fake batch with D
-#         output = netD(fake.detach()).view(-1)
-#         # Calculate D's loss on the all-fake batch
-#         errD_fake = criterion(output, label)
-#         # Calculate the gradients for this batch, accumulated (summed) with previous gradients
-#         errD_fake.backward()
-#         D_G_z1 = output.mean().item()
-#         # Compute error of D as sum over the fake and the real batches
-#         errD = errD_real + errD_fake
-#         # Update D
-#         optimizerD.step()
-#
-#         ############################
-#         # (2) Update G network: maximize log(D(G(z)))
-#         ###########################
-#         netG.zero_grad()
-#         label.fill_(real_label)  # fake labels are real for generator cost
-#         # Since we just updated D, perform another forward pass of all-fake batch through D
-#         output = netD(fake).view(-1)
-#         # Calculate G's loss based on this output
-#         errG = criterion(output, label)
-#         # Calculate gradients for G
-#         errG.backward()
-#         D_G_z2 = output.mean().item()
-#         # Update G
-#         optimizerG.step()
-#
-#         # Output training stats
-#         if i % 50 == 0:
-#             print('[%d/%d][%d/%d]\tLoss_D: %.4f\tLoss_G: %.4f\tD(x): %.4f\tD(G(z)): %.4f / %.4f'
-#                   % (epoch, num_epochs, i, len(dataloader),
-#                      errD.item(), errG.item(), D_x, D_G_z1, D_G_z2))
-#
-#         # Save Losses for plotting later
-#         G_losses.append(errG.item())
-#         D_losses.append(errD.item())
-#
-#         # Check how the generator is doing by saving G's output on fixed_noise
-#         if (iters % 500 == 0) or ((epoch == num_epochs - 1) and (i == len(dataloader) - 1)):
-#             with torch.no_grad():
-#                 fake = netG(fixed_noise).detach().cpu()
-#             img_list.append(vutils.make_grid(fake, padding=2, normalize=True))
-#
-#         iters += 1
-
-
-# plt.figure(figsize=(10, 5))
-# plt.title("Generator and Discriminator Loss During Training")
-# plt.plot(G_losses, label="G")
-# plt.plot(D_losses, label="D")
-# plt.xlabel("iterations")
-# plt.ylabel("Loss")
-# plt.legend()
-# plt.show()
-
-
 def tensor_to_image(tensor):
     import PIL
 
@@ -250,34 +138,41 @@ def tensor_to_image(tensor):
         tensor = tensor[0]
     return PIL.Image.fromarray(tensor)
 
-
-# fake_min, fake_max = fake.min(), fake.max()
-# fake_minmax = fake_max - fake_min
-#
-# fake1 = (fake - fake_min) / fake_minmax
-
-
 # os.mkdir('fake_cats')
 
-# for i, fk in enumerate(fake):
-# # for i, fk in enumerate(fake1):
-#     vutils.save_image(fk, f'fake_cats/{i}.png')
 
-worry = 'worry_cats_generator.pb'
-torch.save(netG, worry)
+worry = 'models/worry_cats_generator.pb'
+empty = 'models/empty_cats_generator.pb'
+neutral = 'models/neutral_cats_generator.pb'
+relief = 'models/relief_cats_generator.pb'
+love = 'models/love_cats_generator.pb'
+surprise = 'models/surprise_cats_generator.pb'
+sadness = 'models/sadness_cats_generator.pb'
 
-netG1 = torch.load(worry)
-netG1.eval()
+def execute(emotion):
+    global netG1
+    if emotion == "empty":
+        netG1 = torch.load(worry)
+    elif emotion == "neutral":
+        netG1 = torch.load(worry)
+    elif emotion == "relief":
+        netG1 = torch.load(worry)
+    elif emotion == "love" or emotion == "hapiness" or emotion == "fun":
+        netG1 = torch.load(worry)
+    elif emotion == "surprise" or emotion == "enthusiasm":
+        netG1 = torch.load(worry)
+    elif emotion == "sadness" or emotion == "boredom":
+        netG1 = torch.load(worry)
+    elif emotion == "worry" or emotion == "anger" or emotion == "hate":
+        netG1 = torch.load(worry)
 
-fixed_noise = torch.randn(1, nz, 1, 1, device=device)
-fake = netG(fixed_noise).detach().cpu()
+    netG1.eval()
+    fixed_noise = torch.randn(1, nz, 1, 1, device=device)
+    fake = netG1(fixed_noise).detach().cpu()
+    fake_min, fake_max = fake.min(), fake.max()
+    fake_minmax = fake_max - fake_min
+    fake1 = (fake - fake_min) / fake_minmax
+    img = vutils.make_grid(fake1, padding=2, normalize=True)
+    vutils.save_image(img, f'fake_cats/{time.time()}.png')
 
-fake_min, fake_max = fake.min(), fake.max()
-fake_minmax = fake_max - fake_min
-
-fake1 = (fake - fake_min) / fake_minmax
-
-img = vutils.make_grid(fake1, padding=2, normalize=True)
-
-
-vutils.save_image(img, f'fake_cats/{time.time()}.png')
+execute("boredom")
