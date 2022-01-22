5f00640bf4ab217197dd5801fa0b22f5529c0877
mmmlpmsw <mmmlpmsw@gmail.com>
2022-01-22 16:42:00 +0300

    Fix jar for bot

--- telegram_bot/src/main/kotlin/ifmo/mmmlpmsw/cloud/bot/CatBotProperties.kt
+++ telegram_bot/src/main/kotlin/ifmo/mmmlpmsw/cloud/bot/CatBotProperties.kt
@@ -1,16 +1,15 @@
 package ifmo.mmmlpmsw.cloud.bot
 
-import java.io.FileInputStream
-import java.util.Properties;
+import java.util.*
 
 class CatBotProperties {
     companion object {
-        private val PROPERTIES_FILE = "bot.properties"
+        private val PROPERTIES_FILE = "/bot.properties"
         private var props: Properties = Properties()
 
         init {
             try {
-                props.load(this::class.java.classLoader.getResourceAsStream(PROPERTIES_FILE))
+                props.load(this::class.java.getResourceAsStream(PROPERTIES_FILE))
             } catch (e: NullPointerException) {
                 System.err.println("Settings file '$PROPERTIES_FILE' not found")
             }
