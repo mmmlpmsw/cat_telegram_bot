350e21a681a3acd9ef28089ce8214bb78138fe87
mmmlpmsw <mmmlpmsw@gmail.com>
2022-01-24 06:45:20 +0300

    Change serialization of models

--- python/2nd/application.py
+++ python/2nd/application.py
@@ -8,35 +8,13 @@ import torchvision.transforms.functional as tvtf
 from flask import Flask
 
 
-manualSeed = 999
-# manualSeed = random.randint(1, 10000) # use if you want new results
-# print("Random Seed: ", manualSeed)
-# random.seed(manualSeed)
-# torch.manual_seed(manualSeed)
-
-workers = 2
-batch_size = 128
-image_size = 64
 nc = 3
 nz = 100
 ngf = 64
-ndf = 64
-num_epochs = 100
-lr = 0.0002
-beta1 = 0.5
 ngpu = 1
 
 
 device = torch.device("cuda:0" if (torch.cuda.is_available() and ngpu > 0) else "cpu")
-
-
-def weights_init(m):
-    classname = m.__class__.__name__
-    if classname.find('Conv') != -1:
-        nn.init.normal_(m.weight.data, 0.0, 0.02)
-    elif classname.find('BatchNorm') != -1:
-        nn.init.normal_(m.weight.data, 1.0, 0.02)
-        nn.init.constant_(m.bias.data, 0)
 
 
 class Generator(nn.Module):
@@ -70,75 +48,40 @@ class Generator(nn.Module):
         return self.main(input)
 
 
-netG = Generator(ngpu).to(device)
-
-if (device.type == 'cuda') and (ngpu > 1):
-    netG = nn.DataParallel(netG, list(range(ngpu)))
-
-netG.apply(weights_init)
-
-
-class Discriminator(nn.Module):
-    def __init__(self, ngpu):
-        super(Discriminator, self).__init__()
-        self.ngpu = ngpu
-        self.main = nn.Sequential(
-            # input is (nc) x 64 x 64
-            nn.Conv2d(nc, ndf, 4, 2, 1, bias=False),
-            nn.LeakyReLU(0.2, inplace=True),
-            # state size. (ndf) x 32 x 32
-            nn.Conv2d(ndf, ndf * 2, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(ndf * 2),
-            nn.LeakyReLU(0.2, inplace=True),
-            # state size. (ndf*2) x 16 x 16
-            nn.Conv2d(ndf * 2, ndf * 4, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(ndf * 4),
-            nn.LeakyReLU(0.2, inplace=True),
-            # state size. (ndf*4) x 8 x 8
-            nn.Conv2d(ndf * 4, ndf * 8, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(ndf * 8),
-            nn.LeakyReLU(0.2, inplace=True),
-            # state size. (ndf*8) x 4 x 4
-            nn.Conv2d(ndf * 8, 1, 4, 1, 0, bias=False),
-            nn.Sigmoid()
-        )
-
-    def forward(self, input):
-        return self.main(input)
-
-
-netD = Discriminator(ngpu).to(device)
-if (device.type == 'cuda') and (ngpu > 1):
-    netD = nn.DataParallel(netD, list(range(ngpu)))
-
-netD.apply(weights_init)
-
-
 emotion_paths = {
     'worry': 'models/worry_cats_generator.pb',
     'anger': 'models/worry_cats_generator.pb',
     'hate': 'models/worry_cats_generator.pb',
-    'empty': 'models/empty_cats_generator.pb',
+    # 'empty': 'models/empty_cats_generator.pb',
     'neutral': 'models/neutral_cats_generator.pb',
-    'relief': 'models/relief_cats_generator.pb',
-    'love': 'models/love_cats_generator.pb',
-    'happiness': 'models/love_cats_generator.pb',
-    'fun': 'models/love_cats_generator.pb',
-    'surprise': 'models/surprise_cats_generator.pb',
-    'enthusiasm': 'models/surprise_cats_generator.pb',
-    'sadness': 'models/sadness_cats_generator.pb',
-    'boredom': 'models/sadness_cats_generator.pb',
+    # 'relief': 'models/relief_cats_generator.pb',
+    # 'love': 'models/love_cats_generator.pb',
+    # 'happiness': 'models/love_cats_generator.pb',
+    # 'fun': 'models/love_cats_generator.pb',
+    # 'surprise': 'models/surprise_cats_generator.pb',
+    # 'enthusiasm': 'models/surprise_cats_generator.pb',
+    # 'sadness': 'models/sadness_cats_generator.pb',
+    # 'boredom': 'models/sadness_cats_generator.pb',
 }
 
 
+emotion_generators = {}
+
+
+def init_generators():
+    for emotion, path in emotion_paths.items():
+        netG = Generator(ngpu).to(device)
+        netG.load_state_dict(torch.load(path))
+        netG.eval()
+
+        emotion_generators[emotion] = netG
+
+
 def execute(emotion):
-    global netG1
+    netG = emotion_generators[emotion]
 
-    netG1 = torch.load(emotion_paths[emotion])
-
-    netG1.eval()
     fixed_noise = torch.randn(1, nz, 1, 1, device=device)
-    fake = netG1(fixed_noise).detach().cpu()
+    fake = netG(fixed_noise).detach().cpu()
     fake = fake[0]
 
     fake_min, fake_max = fake.min(), fake.max()
@@ -147,6 +90,8 @@ def execute(emotion):
 
     return tvtf.to_pil_image(fake, mode='RGB')
 
+
+init_generators()
 
 # webapp
 
